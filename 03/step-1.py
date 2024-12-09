9395471f2aa75b234c262e9205fdfc34a1bedd18
Brian Anderson <brian@gaincompliance.com>
2024-12-09 12:28:57 -0800

    day 3 part 1

---
+++ 03/step-1.py
@@ -0,0 +1,17 @@
+import re
+import os
+
+input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
+with open(input_file_path, "r") as f:
+    input = f.read()
+
+sum = 0
+for [left, right] in re.findall(r"mul\((\d+),(\d+)\)", input):
+    product = int(left) * int(right)
+    # print(f"{left} * {right} = {product}")
+
+    sum += product
+
+print(f"Sum: {sum}")
