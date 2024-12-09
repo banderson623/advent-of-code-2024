a4dbae9d5e95e07918b7ea01a7bd23dac90d05fa
Brian Anderson <brian@gaincompliance.com>
2024-12-09 13:45:07 -0800

    day 3 step 2 refactor

--- 03/step-2.py
+++ 03/step-2.py
@@ -13,27 +13,27 @@ dont_starts = [(m.start(0)) for m in re.finditer(r"don't\(\)", input)]
 do_starts = [(m.start(0)) for m in re.finditer(r"do\(\)", input)]
 
 including = True
-enabled_input = ""
+only_enabled_input = ""
 
 # Walk through the input character by character
 for index, character in enumerate(input):
     if including:
-        enabled_input += character
+        only_enabled_input += character
         if index in dont_starts:
             # Just so you can read the output nicely
-            enabled_input += "on't()"
+            only_enabled_input += "on't()"
             including = False
     else:
-        enabled_input += "-"
+        only_enabled_input += "-"
         if index in do_starts:
             # Just so you can read the output nicely
-            enabled_input += "d"
+            only_enabled_input += "d"
             including = True
 
 # print(enabled_input)
 
 sum = 0
-for [left, right] in re.findall(r"mul\((\d+),(\d+)\)", enabled_input):
+for [left, right] in re.findall(r"mul\((\d+),(\d+)\)", only_enabled_input):
     product = int(left) * int(right)
     sum += product
 
