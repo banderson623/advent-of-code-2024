8092804a258cf46b735f384060f17d09f179e7b5
Brian Anderson <brian@gaincompliance.com>
2024-12-10 21:06:06 -0800

    cleaned up a bit

--- 06/06-01.py
+++ 06/06-01.py
@@ -14,9 +14,9 @@ input = """
 ......#...
 """.strip()
 
-input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
-with open(input_file_path, "r") as f:
-    input = f.read()
+# input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
+# with open(input_file_path, "r") as f:
+#     input = f.read()
 
 map = []
 
@@ -51,67 +51,67 @@ def take_a_step(starting_map):
     height = len(map)
     width = len(map[0])
 
-    position_changed = False
     guard_on_map = True
+    guard_found = False
 
     for y, row in enumerate(map):
-        for x, col in enumerate(row):
-            match col:
-                case Guard.Up:
-                    if y - 1 < 0:
-                        map[y][x] = "X"
-                        guard_on_map = False
-                    elif map[y - 1][x] in MOVABLE_SPACES:
-                        position_changed = True
-                        map[y - 1][x] = Guard.Up
-                        map[y][x] = "X"
-                    else:
-                        map[y][x] = Guard.Right
+        for x, tile in enumerate(row):
+            if not guard_found:
+                match tile:
+                    case Guard.Up:
+                        if y - 1 < 0:
+                            map[y][x] = "X"
+                            guard_on_map = False
+                        elif map[y - 1][x] in MOVABLE_SPACES:
+                            guard_found = True
+                            map[y - 1][x] = Guard.Up
+                            map[y][x] = "X"
+                        else:
+                            map[y][x] = Guard.Right
 
-                case Guard.Right:
-                    if x + 1 >= width:
-                        map[y][x] = "X"
-                        guard_on_map = False
-                    elif map[y][x + 1] in MOVABLE_SPACES:
-                        position_changed = True
-                        map[y][x + 1] = Guard.Right
-                        map[y][x] = "X"
-                    else:
-                        map[y][x] = Guard.Down
+                    case Guard.Right:
+                        if x + 1 >= width:
+                            map[y][x] = "X"
+                            guard_on_map = False
+                        elif map[y][x + 1] in MOVABLE_SPACES:
+                            guard_found = True
+                            map[y][x + 1] = Guard.Right
+                            map[y][x] = "X"
+                        else:
+                            map[y][x] = Guard.Down
 
-                case Guard.Down:
-                    if y + 1 >= height:
-                        map[y][x] = "X"
-                        guard_on_map = False
-                    elif map[y + 1][x] in MOVABLE_SPACES:
-                        position_changed = True
-                        map[y + 1][x] = Guard.Down
-                        map[y][x] = "X"
-                    else:
-                        map[y][x] = Guard.Left
+                    case Guard.Down:
+                        if y + 1 >= height:
+                            map[y][x] = "X"
+                            guard_on_map = False
+                        elif map[y + 1][x] in MOVABLE_SPACES:
+                            guard_found = True
+                            map[y + 1][x] = Guard.Down
+                            map[y][x] = "X"
+                        else:
+                            map[y][x] = Guard.Left
 
-                case Guard.Left:
-                    if x - 1 < 0:
-                        map[y][x] = "X"
-                        guard_on_map = False
-                    elif map[y][x - 1] in MOVABLE_SPACES:
-                        position_changed = True
-                        map[y][x - 1] = Guard.Left
-                        map[y][x] = "X"
-                    else:
-                        map[y][x] = Guard.Up
+                    case Guard.Left:
+                        if x - 1 < 0:
+                            map[y][x] = "X"
+                            guard_on_map = False
+                        elif map[y][x - 1] in MOVABLE_SPACES:
+                            guard_found = True
+                            map[y][x - 1] = Guard.Left
+                            map[y][x] = "X"
+                        else:
+                            map[y][x] = Guard.Up
 
     return guard_on_map
 
 
 prettyPrint(map)
-
 guard_on_map = True
 
 while guard_on_map:
-    # for i in range(50):
+
+    time.sleep(0.1)
     # clear the screen
-    time.sleep(0.01)
     print("\033c")
 
     guard_on_map = take_a_step(map)
