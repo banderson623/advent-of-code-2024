86c24c2655f01ae6f83099af11d690baecd79166
Brian Anderson <brian@gaincompliance.com>
2024-12-15 08:46:50 -0800

    day 10 part 1 complete

--- 10/10-01.py
+++ 10/10-01.py
@@ -19,20 +19,27 @@ input = """
 # .....01
 # """
 
+import os
 
-trailheads = []
-peaks = []
+input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
+with open(input_file_path, "r") as f:
+    input = f.read().strip()
+
+
+all_trailheads = []
+all_peaks = []
 scores = {}
 map = []
+
 
 for [y, row] in enumerate(input.strip().split("\n")):
     row_data = []
     for [x, item] in enumerate(row):
         if item == "0":
-            trailheads.append((x, y))
+            all_trailheads.append((x, y))
             scores[(x, y)] = 0
         elif item == "9":
-            peaks.append((x, y))
+            all_peaks.append((x, y))
         row_data.append(item)
     map.append(row_data)
 
@@ -47,9 +54,9 @@ def prettyPrint(map):
     print("+" + "-" * len(map[0]) + "+")
 
 
-prettyPrint(map)
-print("Trailheads", trailheads)
-print("Peaks", peaks)
+# prettyPrint(map)
+print("Trailheads", all_trailheads)
+print("Peaks", all_peaks)
 
 
 def prettPrintPath(path, map):
@@ -64,7 +71,7 @@ def prettPrintPath(path, map):
     prettyPrint(this_map)
 
 
-def backtracking_path_finding(map, location, current_path, all_paths, trailheads):
+def backtracking_path_finding(map, location, current_path, all_paths, goals):
     x, y = location
 
     if location in current_path:
@@ -74,8 +81,8 @@ def backtracking_path_finding(map, location, current_path, all_paths, trailheads
 
     current_elevation = int(map[y][x])
 
-    # I have arrived at a trailhead, yay!
-    if location in trailheads:
+    # We arrived at our destination!
+    if location in goals:
         all_paths.append(current_path)
 
     else:
@@ -86,34 +93,35 @@ def backtracking_path_finding(map, location, current_path, all_paths, trailheads
             if next_x < 0 or next_x >= WIDTH or next_y < 0 or next_y >= HEIGHT:
                 continue
 
+            # can't step off route on sparse maps
             if map[next_y][next_x] == ".":
                 continue
 
             next_evelvation = int(map[next_y][next_x])
 
-            if next_evelvation == current_elevation - 1:
-                # print(f"...going to {next} (from {current_elevation} to {next_evelvation})")
-                backtracking_path_finding(map, next, current_path.copy(), all_paths, trailheads)
-
-    # current_path.pop()
+            if next_evelvation == current_elevation + 1:
+                backtracking_path_finding(map, next, current_path.copy(), all_paths, goals)
 
 
 # let's walk down from the peaks and see where it goes
-for peak in peaks:
-    x, y = peak
+for trailhead in all_trailheads:
 
     all_paths = []
-    backtracking_path_finding(map, peak, [], all_paths, trailheads)
-    print("------------------------------------------")
-    print("Done walking down from peak", peak, "Paths:", len(all_paths))
-    # print("Paths:", all_paths)
+    backtracking_path_finding(map, trailhead, [], all_paths, all_peaks)
+    # print("Done walking from trailhead", trailhead, "Paths to Peaks:", len(all_paths))
+
+    peaks_visited = set()
 
     for path in all_paths:
-        trailhead = path[-1]
-        for location in path:
-            if location in trailheads:
-                scores[trailhead] += 1
+        trailhead = path[0]
+        peak = path[-1]
 
-        # prettPrintPath(path, map)
+        if peak not in peaks_visited:
+            peaks_visited.add(peak)
+            scores[trailhead] += 1
 
-print("Scores", scores, sum(scores.values()))
+        # if trailhead == (17, 6):
+        #     prettPrintPath(path, map)
+
+# print("Scores", scores)
+print("Sum of scores", sum(scores.values()))
